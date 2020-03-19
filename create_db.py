7be2ee033019cae270cfce1944f76545f2820d2d
hypnoZzzz <59202624+hypnoZzzz@users.noreply.github.com>
2020-03-19 14:26:50 +0300

    Add files via upload

---
+++ create_db.py
@@ -0,0 +1,17 @@
+#Создаем соединение с базой(с помощью sqlite) и все нужные таблицы (они есть благодаря объекту metadata)
+
+from app.db import metadata, vote_results
+import sqlalchemy as sa
+
+if __name__ == '__main__':
+    engine = sa.create_engine('sqlite:///my_db.sqlite')
+    metadata.create_all(engine)
+
+    with engine.begin () as connection:
+        for i, animal in enumerate (['cats', 'dogs', 'parrots'], start = 1):
+            statement = vote_results.insert().values (
+                id=i,
+                name=animal,
+                votes=0
+            )
+            connection.execute(statement)
